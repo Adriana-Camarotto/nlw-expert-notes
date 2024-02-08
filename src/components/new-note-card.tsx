ff2ae3518d4daab46d53cdde0a0c7abead6e0b3b
Adriana Camarotto <adriana.camarotto@gmail.com>
2024-02-08 20:58:56 +0000

    created form to handle and save the new note

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -1,9 +1,34 @@
 import * as Dialog from "@radix-ui/react-dialog";
 import { X } from "lucide-react";
+import { ChangeEvent, useState } from "react";
+
 export function NewNoteCard() {
+  const [shouldShowOnboarding, setShouldShowOnboarding] = useState(true);
+  const [content, setContent] = useState('');
+
+  function handleStartEditor() {
+    setShouldShowOnboarding(false);
+  }
+
+  //when the user erases the keyboard it will show the onboarding button
+  function handleContentChanged(event: ChangeEvent<HTMLTextAreaElement>) {
+    setContent(event.target.value);
+    
+    if (event.target.value === '') {
+      setShouldShowOnboarding(true);
+    }
+  }
+
+  //saving the new note
+  function handleSaveNote(event: FormEvent) {
+    event.preventDefault();
+    console.log(content);
+  }
+
+
   return (
     <Dialog.Root>
-      <Dialog.Trigger className="rounded-md flex flex-col bg-slate-700 p-5 gap-3 text-left">
+      <Dialog.Trigger className="rounded-md flex flex-col bg-slate-700 p-5 gap-3 autline-none text-left hover:ring-2 hover:ring-slate-600 focus:ring-2 focus:ring-lime-400">
         <span className="text-sm font-medium text-slate-200">New note</span>
         <p className="text-sm leading-6 text-slate-400">
           Record an audio note that will be automatically converted to text.
@@ -14,24 +39,45 @@ export function NewNoteCard() {
         <Dialog.Overlay className="inset-0 fixed bg-black/50" />
         <Dialog.Content className="fixed overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col autline-none">
           <Dialog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
-            <X className="size-5"/>
+            <X className="size-5" />
           </Dialog.Close>
-          
-          <div className="flex flex-1 flex-col gap-3 p-5">
-            <span className="text-sm font-medium text-slate-300">
-              
-            </span>
-            <p className="text-sm leading-6 text-slate-400">
-              
-            </p>
-          </div>
 
-          <button 
-            type="button"
-            className="w-full bg-slate-800 py-4 text-center text-sm text-slate-300 outline-none font-medium group"
-          >
-            Would you like to <span className="text-red-400 group-hover:underline">delete this note</span>?
-          </button>
+          <form onSubmit={handleSaveNote} className="flex-1 flex flex-col">
+            <div className="flex flex-1 flex-col gap-3 p-5">
+              <span className="text-sm font-medium text-slate-300">
+                New note
+              </span>
+              {shouldShowOnboarding ? (
+                <p className="text-sm leading-6 text-slate-400">
+                  Begin by{" "}
+                  <button className="font-medium text-lime-400 hover:underline">
+                    recording an audio note
+                  </button>{" "}
+                  or, if you prefer,{" "}
+                  <button
+                    onClick={handleStartEditor}
+                    className="font-medium text-lime-400 hover:underline"
+                  >
+                    simply use text
+                  </button>
+                  .
+                </p>
+              ) : (
+                <textarea
+                  autoFocus
+                  className="text-sm leading-6 text-slate-400 bg-transparent resize-none flex-1 outline-none"
+                  onChange={handleContentChanged} //when the use erase the text come back to the onboard
+                />
+              )}
+            </div>
+
+            <button
+              type="submit"
+              className="w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font-medium hover:bg-lime-500"
+            >
+              Save note
+            </button>
+          </form>
         </Dialog.Content>
       </Dialog.Portal>
     </Dialog.Root>
