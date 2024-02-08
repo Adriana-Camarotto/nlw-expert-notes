721385ba7c57fc493c084e18091751462d744d88
Adriana Camarotto <adriana.camarotto@gmail.com>
2024-02-08 10:35:53 +0000

    created modal

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,3 +1,5 @@
+import * as Dialog from "@radix-ui/react-dialog";
+
 interface NoteCardProps {
   note: {
     date: Date;
@@ -5,16 +7,23 @@ interface NoteCardProps {
   };
 }
 
-export function NoteCard({note}: NoteCardProps) {
+export function NoteCard({ note }: NoteCardProps) {
   return (
-    <button className="rounded-md text-left flex flex-col bg-slate-800 p-5 gap-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus:ring-2 focus:ring-lime-400">
-      <span className="text-sm font-medium text-slate-200">
-        {note.date.toISOString()}
-      </span>
+    <Dialog.Root>
+      <Dialog.Trigger className="rounded-md text-left flex flex-col bg-slate-800 p-5 gap-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus:ring-2 focus:ring-lime-400">
+        <span className="text-sm font-medium text-slate-200">
+          {note.date.toISOString()}
+        </span>
 
-      <p className="text-sm leading-6 text-slate-300">{note.content}</p>
+        <p className="text-sm leading-6 text-slate-300">{note.content}</p>
 
-      <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
-    </button>
-  );
+        <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
+      </Dialog.Trigger>
+
+      <Dialog.Portal>
+        <Dialog.Overlay className="inset-0 fixed bg-black/50" />
+        <Dialog.Content className=" z-0 bg-white absolute">Hello!</Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
+  )
 }
