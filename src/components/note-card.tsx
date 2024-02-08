eb41bbac92afcf251a8e64ad9c08fde30b08ce0f
Adriana Camarotto <adriana.camarotto@gmail.com>
2024-02-08 09:48:12 +0000

    added props to notes cards

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,14 +1,20 @@
-export function NoteCard() {
+interface NoteCardProps {
+  note: {
+    date: Date;
+    content: string;
+  };
+}
+
+export function NoteCard({note}: NoteCardProps) {
   return (
-    <div className="rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative">
-      <span className="text-sm font-medium text-slate-200">2 days ago</span>
-      <p className="text-sm leading-6 text-slate-300">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse est
-        dolorum culpa libero obcaecati odio necessitatibus quae earum minus
-        corporis, eligendi optio tenetur! Molestias accusamus dolores vitae
-        deleniti quibusdam esse?
-      </p>
+    <button className="rounded-md text-left flex flex-col bg-slate-800 p-5 gap-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus:ring-2 focus:ring-lime-400">
+      <span className="text-sm font-medium text-slate-200">
+        {note.date.toISOString()}
+      </span>
+
+      <p className="text-sm leading-6 text-slate-300">{note.content}</p>
+
       <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
-    </div>
+    </button>
   );
 }
