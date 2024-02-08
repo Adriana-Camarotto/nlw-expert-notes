92bf8b5840b0afe0371d4d3db556352d50e96590
Adriana Camarotto <adriana.camarotto@gmail.com>
2024-02-08 17:22:57 +0000

    adeed modal to the new-note-card.

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -1,12 +1,39 @@
-export function NewNoteCard () {
-    return (
-        <div className="rounded-md bg-slate-700 p-5">
-          <span className="text-sm font-medium text-slate-200">New note</span>
-          <p className="text-sm leading-6 text-slate-400">
-            Record an audio note that will be automatically converted to text.
-          </p>
-        </div>
-    )
+import * as Dialog from "@radix-ui/react-dialog";
+import { X } from "lucide-react";
+export function NewNoteCard() {
+  return (
+    <Dialog.Root>
+      <Dialog.Trigger className="rounded-md flex flex-col bg-slate-700 p-5 gap-3 text-left">
+        <span className="text-sm font-medium text-slate-200">New note</span>
+        <p className="text-sm leading-6 text-slate-400">
+          Record an audio note that will be automatically converted to text.
+        </p>
+      </Dialog.Trigger>
 
+      <Dialog.Portal>
+        <Dialog.Overlay className="inset-0 fixed bg-black/50" />
+        <Dialog.Content className="fixed overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col autline-none">
+          <Dialog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
+            <X className="size-5"/>
+          </Dialog.Close>
+          
+          <div className="flex flex-1 flex-col gap-3 p-5">
+            <span className="text-sm font-medium text-slate-300">
+              
+            </span>
+            <p className="text-sm leading-6 text-slate-400">
+              
+            </p>
+          </div>
 
+          <button 
+            type="button"
+            className="w-full bg-slate-800 py-4 text-center text-sm text-slate-300 outline-none font-medium group"
+          >
+            Would you like to <span className="text-red-400 group-hover:underline">delete this note</span>?
+          </button>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
+  );
 }
